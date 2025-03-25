ba0728952a0bbb3c39d380bede21bace4daea930
axtlco <yhc407@kaist.ac.kr>
2025-03-25 11:26:15 +0900

    init

---
+++ MyWidgetProvider.kt
@@ -0,0 +1,22 @@
+package com.example.mywidget
+
+import android.appwidget.AppWidgetManager 
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import kotlinx.cooutines.*
+import java.net.URL
+import org.json.JSONObject
+
+class MyWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(){
+
+    }
+
+    private fun updateWidgetAsync() {
+
+    }
+
+
+}
---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS 
+
+app = Flask(__name__)
+CORS(app) 
+
+@app.route("/message")
+def get_message():
+    return jsonify({"message" : "Hello"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
